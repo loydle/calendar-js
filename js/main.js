19cd91e7fc17b2463e7f25163085cdfc93996062
loydle <matthieu@digitalsc.org>
2018-11-03 21:33:47 +0100

    calendar get today

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
-var today = (function() {
-    var today = new Date();
-    return {
-        day: today.getDate(),
-        month: today.getMonth() + 1,
-        year: today.getFullYear(),
+function calendar() {
+
+    // default value;
+    this.year = getYear();
+    this.month = getMonth();
+    this.days = getDays(getDaysOfMonth(this.month));
+
+
+    return this;
+}
+
+function getDaysOfMonth(month) {
+    var month = month || [];
+    if (month === 'February') {
+        return function getDaysOfFebruary() {
+            //
+        }
+    } else if (
+        ['January', 'March', 'May', 'July', 'August', 'October', 'December']
+        .filter(m => m === month)[0]
+    ) {
+        return 31;
+    } else {
+        return 30;
     }
-}())
-console.log(today)
+};
+
+function getDays(numDays) {
+    var days = [];
+    for (let i = 1; i <= numDays; i++) {
+        days.push(i);
+    };
+    return days;
+};
+
+function getYear() {
+    var today = new Date();
+    return today.getFullYear();
+}
+
+function getMonth() {
+    var today = new Date();
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    return months[today.getMonth()];
+}
+
+console.log(new calendar())
