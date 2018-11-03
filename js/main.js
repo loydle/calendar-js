70f70c022c10d87189f428d16bf0510159d5b53d
loydle <matthieu@digitalsc.org>
2018-11-03 21:44:04 +0100

    calendar renderToday wip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function calendar() {
 
     // default value;
+    this.today = new Date().getDate();
     this.year = getYear();
     this.month = getMonth();
     this.days = getDays(getDaysOfMonth(this.month));
@@ -44,4 +45,20 @@ function getMonth() {
     return months[today.getMonth()];
 }
 
-console.log(new calendar())
+console.log(new calendar());
+
+renderToday(document.getElementById('calendar'));
+renderDays(document.getElementById('calendar'), new calendar());
+
+function renderDays(calendarHTMLElm, calendarObj) {
+    for (let i = 1; i <= calendarObj.days.length; i++) {
+        let div = document.createElement('div');
+        div.innerText = i;
+        div.classList.add('day')
+        calendarHTMLElm.querySelector('section.days').appendChild(div);
+    }
+}
+
+function renderToday(calendarHTMLElm) {
+    // var daysNode = querySelectorAll('')
+}
