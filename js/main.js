8ddace80b52d88f4c6760d6407b7aabdc60694c8
loydle <matthieu@digitalsc.org>
2018-11-03 23:12:12 +0100

    refactor

--- js/main.js
+++ js/main.js
@@ -1,13 +1,73 @@
-function calendar() {
+var Calendar =
+    new Calendar(new Day(new Date()));
 
+renderCalendar(
+    document.getElementById('calendar'),
+    Calendar
+);
+
+
+function renderCalendar(calendarNode, CalendarObj) {
+
+    renderDays(calendarNode, CalendarObj);
+    renderTodayActiveDay(calendarNode, CalendarObj);
+    renderTodayMonth(calendarNode, CalendarObj);
+
+    function renderDays(calendarHTMLElm, Calendar) {
+        for (let i = 1; i <= Calendar.days.length; i++) {
+            let div = document.createElement('div');
+            div.dataset.day = i;
+            div.innerText = div.dataset.day;
+            div.classList.add('day');
+            calendarHTMLElm.querySelector('section.days').appendChild(div);
+        }
+    }
+
+    function renderTodayActiveDay(calendarHTMLElm, Calendar) {
+        var daysNode = calendarHTMLElm.querySelectorAll('div.day');
+        for (var i = 0; i < daysNode.length; i++) {
+            if (daysNode[i].dataset.day == Calendar.today) {
+                daysNode[i].classList.add('today');
+            } else {
+                daysNode[i].classList.remove('today');
+            }
+        }
+
+    }
+
+    function renderTodayMonth(calendarHTMLElm, Calendar) {
+        var monthNode = calendarHTMLElm.querySelector('.month').innerHTML = Calendar.year + '<br>' + Calendar.month;
+    }
+}
+
+function Day(dayAsDateObj) {
+    this.date = dayAsDateObj;
+    this.tasks = getTasks(dayAsDateObj);
+
+    function getTasks(dayAsDateObj) {
+        return [new Task("11:00", "Meeting")]
+    }
+    console.log(this);
+}
+
+function Task(time, name) {
+    this.time = time || "00:00";
+    this.name = name || " ";
+    console.log(this);
+    return this;
+}
+
+
+function Calendar(Day) {
     // default value;
-    var date = new Date();
+    var date = Day.date;
+    this.date = date;
     this.today = date.getDate();
     this.year = getYear();
     this.month = getMonth();
     this.days = getDays(getDaysOfMonth(this.month));
     this.dayOfWeek = getDayOfWeek(date);
-
+    console.log(this);
     return this;
 }
 
@@ -49,36 +109,4 @@ function getMonth() {
     var today = new Date();
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     return months[today.getMonth()];
-}
-
-console.log(new calendar());
-var calendar = new calendar();
-renderDays(document.getElementById('calendar'), calendar);
-renderTodayActiveDay(document.getElementById('calendar'), calendar);
-renderTodayMonth(document.getElementById('calendar'), calendar);
-
-function renderDays(calendarHTMLElm, calendarObj) {
-    for (let i = 1; i <= calendarObj.days.length; i++) {
-        let div = document.createElement('div');
-        div.dataset.day = i;
-        div.innerText = div.dataset.day;
-        div.classList.add('day');
-        calendarHTMLElm.querySelector('section.days').appendChild(div);
-    }
-}
-
-function renderTodayActiveDay(calendarHTMLElm, calendarObj) {
-    var daysNode = calendarHTMLElm.querySelectorAll('div.day');
-    for (var i = 0; i < daysNode.length; i++) {
-        if (daysNode[i].dataset.day == calendarObj.today) {
-            daysNode[i].classList.add('today');
-        } else {
-            daysNode[i].classList.remove('today');
-        }
-    }
-
-}
-
-function renderTodayMonth(calendarHTMLElm, calendarObj) {
-    var monthNode = calendarHTMLElm.querySelector('.month').innerHTML = calendarObj.year + '<br>' + calendarObj.month;
 }
