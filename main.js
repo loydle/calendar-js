379d78aa185226ee77af62f93ea25826e742ab42
loydle <matthieu@digitalsc.org>
2020-11-16 02:27:12 +0100

    refactor: simplify code

--- main.js
+++ main.js
@@ -6,55 +6,64 @@
 */
 class CalendarJS {
     constructor(date) {
-        this.date = (date) ? date : new Date();
-        this.year = this.date.getFullYear();
+        this.date  = date ? date : new Date();
+        this.year  = this.date.getFullYear();
         this.month = this.date.getMonth();
-        this.html = this.getHTML();
+        this.html  = this.getHTML();
     }
 
     getDaysOfMonthAsHTML() {
         let section = document.createElement('section');
         let date = 1;
+        /* Loop over weeks */
         for (let i = 0; i < 6; i += 1) {
-            let ul = document.createElement("ul");
-
-            //creating individual cells, filing them up with data.
+            let ul = document.createElement('ul');
+            /* Loop over days in week */
             for (let j = 0; j < 7; j++) {
+                /* Day is not part of this week */
                 if (i === 0 && j < (new Date(this.date.getFullYear(), this.date.getMonth())).getDay()) {
                     const li = document.createElement('li');
-                    const cellText = document.createTextNode("");
-                    li.appendChild(cellText);
                     ul.appendChild(li);
                 }
+                /* Day is over the week (ex: if day is 32) */
                 else if (date > new Date(this.date.getMonth(), this.date.getFullYear(), 0).getDate()) {
                     break;
                 }
-
+                /* Day is part of the week */
                 else {
                     const li = document.createElement('li');
-                    const cellText = document.createTextNode(date);
+                    /* Day is today, add class .today */
                     if (date === this.date.getDate() && this.year === this.date.getFullYear() && this.month === this.date.getMonth()) {
                         li.classList.add("today");
                     }
-                    li.appendChild(cellText);
+                    /* date is not today but part of the week */
+                    li.appendChild(document.createTextNode(date));
                     ul.appendChild(li);
-                    date++;
+                    date += 1;
                 }
             }
             section.appendChild(ul)
         }
-
         return section;
     }
-
+    prev () {
+        console.log('ok')
+    }
     getHTML() {
         const article = document.createElement('article');
         article.id = 'calendar';
-        article.innerHTML = `
+        article.innerHTML += `
+        <h1>${this.date.toLocaleString('en-EN', { year: 'numeric', month: 'long', day: 'numeric' })}</h1>
         <header>
-        <ul>
-        <li>S</li><li>M</li><li>T</li><li>W</li><li>T</li><li>F</li><li>S</li>
-        </ul>
+            <ul>
+                <li>S</li>
+                <li>M</li>
+                <li>T</li>
+                <li>W</li>
+                <li>T</li>
+                <li>F</li>
+                <li>S</li>
+            </ul>
         </header>
         `;
         article.appendChild(this.getDaysOfMonthAsHTML());
